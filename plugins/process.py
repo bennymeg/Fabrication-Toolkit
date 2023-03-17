c62f1e3c1e4e960be63a7ce2847cf28d6554bee7
Benny Megidish <bennymegk@gmail.com>
2023-03-17 16:02:53 +0200

    fix: solder mask margin as per JLCPCB guidelines #34

--- plugins/process.py
+++ plugins/process.py
@@ -65,7 +65,7 @@ class ProcessManager:
     def generate_gerber(self, temp_dir):
         '''Generate the Gerber files.'''
         settings = self.board.GetDesignSettings()
-        settings.m_SolderMaskMargin = 0
+        settings.m_SolderMaskMargin = 0.05
         settings.m_SolderMaskMinWidth = 0
 
         plot_controller = pcbnew.PLOT_CONTROLLER(self.board)
