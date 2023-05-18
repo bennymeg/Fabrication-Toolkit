1d5a1b244e1ab77e023426ad7992e51604b6718b
Benny Megidish <bennymegk@gmail.com>
2023-05-18 20:06:47 +0300

    feat: orginize gerber archive #15 #61

--- plugins/thread.py
+++ plugins/thread.py
@@ -1,5 +1,6 @@
 import os
 import wx
+import pcbnew
 import shutil
 import tempfile
 import webbrowser
@@ -22,6 +23,12 @@ class ProcessThread(Thread):
         self.report(0)
 
         temp_dir = tempfile.mkdtemp()
+        gerber_dir = os.path.join(temp_dir, 'gerber')
+        assembly_dir = os.path.join(temp_dir, 'assembly')
+
+        os.makedirs(gerber_dir)
+        os.makedirs(assembly_dir)
+
         _, temp_file = tempfile.mkstemp()
         project_directory = os.path.dirname(self.process_manager.board.GetFileName())
         output_path = os.path.join(project_directory, outputFolder)
@@ -29,27 +36,27 @@ class ProcessThread(Thread):
         try:
             # configure and generate gerber
             self.report(5)
-            self.process_manager.generate_gerber(temp_dir)
+            self.process_manager.generate_gerber(gerber_dir)
 
             # generate drill file
             self.report(15)
-            self.process_manager.generate_drills(temp_dir)
+            self.process_manager.generate_drills(gerber_dir)
 
             # generate netlist
             self.report(25)
-            self.process_manager.generate_netlist(temp_dir)
+            self.process_manager.generate_netlist(assembly_dir)
 
             # generate pick and place file
             self.report(40)
-            self.process_manager.generate_positions(temp_dir)
+            self.process_manager.generate_positions(assembly_dir)
 
             # generate BOM file
             self.report(60)
-            self.process_manager.generate_bom(temp_dir)
+            self.process_manager.generate_bom(assembly_dir)
 
             # generate production archive
             self.report(75)
-            temp_file = self.process_manager.generate_archive(temp_dir, temp_file)
+            temp_file = self.process_manager.generate_archive(gerber_dir, temp_file)
         except Exception as e:
             wx.MessageBox(str(e), "Error", wx.OK | wx.ICON_ERROR)
             self.report(-1)
@@ -67,8 +74,27 @@ class ProcessThread(Thread):
                 percent = read_so_far * 1e2 / total_size
                 self.report(75 + percent / 8)
 
-        os.rename(temp_file, os.path.join(temp_dir, gerberArchiveName))
+        # generate gerber name
+        title_block = self.process_manager.board.GetTitleBlock()
+        title = title_block.GetTitle()
+        revision = title_block.GetRevision()
+        company = title_block.GetCompany()
+        file_date = title_block.GetDate()
 
+        if (hasattr(self.process_manager.board, "GetProject") and hasattr(pcbnew, "ExpandTextVars")):
+            project = self.process_manager.board.GetProject()
+            title = pcbnew.ExpandTextVars(title, project)
+            revision = pcbnew.ExpandTextVars(revision, project)
+            company = pcbnew.ExpandTextVars(company, project)
+            file_date = pcbnew.ExpandTextVars(file_date, project)
+
+        filename = os.path.splitext(os.path.basename(self.process_manager.board.GetFileName()))[0]
+        gerberArchiveName = "_".join(("{} {}".format(title or filename, revision or '').strip() + '.zip').split())
+
+        # rename gerber archive
+        os.rename(temp_file, os.path.join(gerber_dir, gerberArchiveName))
+
+        # copy & open cleaned output dir 
         try:
             if os.path.exists(output_path):
                 shutil.rmtree(output_path)
