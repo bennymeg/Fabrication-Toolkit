4c2f39a26ea7bfb95a278fddcf1a2bebefe2f603
Mats Engstrom <mats.engstrom@protonmail.ch>
2023-10-07 20:58:15 +0200

    Use configure value for setting limit of merged parts in BOM

--- plugins/config.py
+++ plugins/config.py
@@ -8,6 +8,7 @@ placementFileName = 'positions.csv'
 bomFileName = 'bom.csv'
 gerberArchiveName = 'gerbers.zip'
 outputFolder = 'production'
+bomRowLimit = 30
 
 plotPlan = [
     ("F.Cu", pcbnew.F_Cu, "Top Layer"),
