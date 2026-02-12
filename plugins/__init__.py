2fda1f7cee9c15caa103933a405b78d7867e9ea9
Tim Goll <github@timgoll.de>
2026-02-12 18:19:46 +0100

    fix CLI on windows

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,8 +1,15 @@
-try:
-    from .plugin import Plugin
-    plugin = Plugin()
-    plugin.register()
-except Exception as e:
-    import logging
-    logger = logging.getLogger()
-    logger.debug(repr(e))
+import sys
+
+# Check if 'pcbnew' is already loaded in a way that suggests a GUI context
+# Or check if we are running in a standalone interpreter
+is_standalone = 'pcbnew' not in sys.modules or __name__ == "__main__"
+
+if not is_standalone:
+    try:
+        from .plugin import Plugin
+        plugin = Plugin()
+        plugin.register()
+    except Exception as e:
+        import logging
+        logger = logging.getLogger()
+        logger.debug(repr(e))
